f26804c62809c8cdcf9f7c85a28b0155d77f1c29
Lazaros Toumanidis <laztoum@waldiez.io>
2026-03-26 12:34:41 +0200

    frontend: update generated assets

---
+++ static/docs/api/rust/trait.impl/clap_builder/derive/trait.CommandFactory.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["wactorz",[["impl CommandFactory for <a class=\"struct\" href=\"wactorz/struct.Args.html\" title=\"struct wactorz::Args\">Args</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[135]}
